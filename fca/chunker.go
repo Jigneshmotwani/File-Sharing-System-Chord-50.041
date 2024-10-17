22edb1fc2f1a24f165600242893a9b67f2138ef3
akashcx <akashchandrasekar@gmail.com>
2024-10-17 20:50:34 +0800

    updated chunker code

--- fca/chunker.go
+++ fca/chunker.go
@@ -1,38 +1,23 @@
 package fca
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
 	"fmt"
 	"io"
-	"math/big"
 	"os"
 	"path/filepath"
-	"sort"
 	"strings"
 )
 
-const (
-	chunkSize    = 1024                                                // 1KB size limit for each chunk, change if needed
-	maxHashValue = "1461501637330902918203684832716283019655932542975" // 2^160 - 1
-)
-
-type Node struct {
-	FolderName string
-	HashValue  *big.Int
-}
-
-// NodeInterval represents the interval assigned to a node
-type NodeInterval struct {
-	Node       Node
-	StartValue *big.Int
-	EndValue   *big.Int
-}
-
-func Chunker() {
+func Chunker() *ChunkInfo {
 	// Paths
-	dataDir := "Data" // Change if needed
+	dataDir := "./Data" // Change if needed
 	var sourceFolder, sourceFile, absPath string
+
+	// Initialize the ChunkInfo struct
+	chunkInfo := &ChunkInfo{
+		ChunkLocations: []string{},
+		Name:           "",
+	}
 
 	// Loop to allow user to switch nodes or select a file
 	for {
@@ -40,7 +25,7 @@ func Chunker() {
 		sourceFolder = promptUserForFolder(dataDir)
 		if sourceFolder == "" {
 			fmt.Println("No valid folder selected.")
-			return
+			return nil
 		}
 
 		sourceFile, absPath = promptUserForFile(dataDir, sourceFolder)
@@ -49,9 +34,11 @@ func Chunker() {
 		}
 		if sourceFile == "" {
 			fmt.Println("No valid file selected.")
-			return
+			return nil
 		}
 
+		// Set the original file name in the ChunkInfo struct
+		chunkInfo.Name = filepath.Base(sourceFile)
 		break // Exit the loop after a valid file is selected
 	}
 
@@ -59,24 +46,24 @@ func Chunker() {
 	file, err := os.Open(sourceFile)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
+		return nil
 	}
 	defer file.Close()
 
-	// Get all the node folders and assign intervals
+	// Get all the node folders and assign finger tables
 	nodeFolders, err := getNodeFolders(dataDir, sourceFolder)
 	if err != nil {
 		fmt.Println("Error retrieving folders:", err)
-		return
+		return nil
 	}
 
 	if len(nodeFolders) == 0 {
 		fmt.Println("No other folders found for distributing chunks.")
-		return
+		return nil
 	}
 
-	// Create and assign intervals to nodes based on hashed folder names
-	nodeIntervals := assignNodeIntervals(nodeFolders)
+	// Create and initialize finger tables for all nodes
+	nodes := initializeFingerTables(nodeFolders)
 
 	buffer := make([]byte, chunkSize)
 	chunkNumber := 1
@@ -91,7 +78,7 @@ func Chunker() {
 		bytesRead, err := file.Read(buffer)
 		if err != nil && err != io.EOF {
 			fmt.Println("Error reading file:", err)
-			return
+			return nil
 		}
 		if bytesRead == 0 {
 			break
@@ -103,99 +90,31 @@ func Chunker() {
 		// Hash the chunk file name using SHA-1 and convert it to a big integer
 		hashedChunkFileName := hashSHA1(chunkFileName)
 		hashedChunkBigInt := hashToBigInt(hashedChunkFileName)
-		// fmt.Printf("Chunk File Name: %s\nSHA-1 Hash: %s\nBigInt: %s\n", chunkFileName, hashedChunkFileName, hashedChunkBigInt)
 
 		// Find the appropriate node based on the chunk's big integer value
-		assignedNode := findAssignedNode(hashedChunkBigInt, nodeIntervals)
+		assignedNode := findSuccessor(hashedChunkBigInt, nodes)
 
 		if assignedNode != nil {
 			// Save the chunk in the assigned node's folder
-			dataDir := "" // Change if needed
-			destinationFolder := filepath.Join(dataDir, assignedNode.Node.FolderName)
+			destinationFolder := filepath.Join("", assignedNode.FolderName)
 			chunkPath := filepath.Join(destinationFolder, chunkFileName)
 			err = writeChunk(chunkPath, buffer[:bytesRead])
 			if err != nil {
 				fmt.Println("Error writing chunk:", err)
-				return
+				return nil
 			}
-			fmt.Printf("Chunk %d assigned to node: %s\n", chunkNumber, assignedNode.Node.FolderName)
+			fmt.Printf("Chunk %d assigned to node: %s\n", chunkNumber, assignedNode.FolderName)
+
+			// Append the chunk's location info to the ChunkLocations field in chunkInfo
+			chunkInfo.ChunkLocations = append(chunkInfo.ChunkLocations, assignedNode.FolderName)
 		} else {
 			fmt.Println("No node found for chunk:", chunkFileName)
 		}
 
 		chunkNumber++
 	}
-}
 
-// assign intervals to nodes based on hashed folder names
-func assignNodeIntervals(nodeFolders []string) []NodeInterval {
-	var nodes []Node
-
-	// Create Node structs with hashed values
-	for _, folder := range nodeFolders {
-		hashedFolderName := hashSHA1(folder)
-		hashedFolderBigInt := hashToBigInt(hashedFolderName)
-		nodes = append(nodes, Node{FolderName: folder, HashValue: hashedFolderBigInt})
-		// fmt.Printf("Node: %s", nodes)
-	}
-
-	// Sort nodes by their hash value
-	sort.Slice(nodes, func(i, j int) bool {
-		return nodes[i].HashValue.Cmp(nodes[j].HashValue) < 0
-	})
-
-	var nodeIntervals []NodeInterval
-	maxHashBigInt := new(big.Int)
-	maxHashBigInt.SetString(maxHashValue, 10)
-
-	for i := 0; i < len(nodes); i++ {
-		start := nodes[i].HashValue
-		var end *big.Int
-
-		if i == len(nodes)-1 {
-			// Last node wraps around to include [nodes[i] -> 2^160-1] and [0 -> first node]
-			end = new(big.Int).Sub(maxHashBigInt, big.NewInt(1))
-		} else {
-			// Set end to the next node's value - 1
-			end = new(big.Int).Sub(nodes[i+1].HashValue, big.NewInt(1))
-		}
-		nodeIntervals = append(nodeIntervals, NodeInterval{
-			Node:       nodes[i],
-			StartValue: start,
-			EndValue:   end,
-		})
-		// Print the interval for all nodes, including the selected node
-		fmt.Printf("Node: %s, Interval: [%s, %s]\n", nodes[i].FolderName, start.String(), end.String())
-	}
-	// Assignment for last node
-	if len(nodes) > 0 {
-		firstNodeStartValue := nodeIntervals[0].StartValue
-		nodeIntervals[len(nodeIntervals)-1].EndValue = maxHashBigInt
-
-		nodeIntervals = append(nodeIntervals, NodeInterval{
-			Node:       nodes[0],
-			StartValue: big.NewInt(0),
-			EndValue:   firstNodeStartValue,
-		})
-	}
-	return nodeIntervals
-}
-
-func findAssignedNode(chunkBigInt *big.Int, nodeIntervals []NodeInterval) *NodeInterval {
-	for _, interval := range nodeIntervals {
-		if isInInterval(chunkBigInt, interval.StartValue, interval.EndValue) {
-			return &interval
-		}
-	}
-	return nil
-}
-
-func isInInterval(value, start, end *big.Int) bool {
-	if start.Cmp(end) <= 0 {
-		return value.Cmp(start) >= 0 && value.Cmp(end) <= 0
-	} else {
-		return value.Cmp(start) >= 0 || value.Cmp(end) <= 0
-	}
+	return chunkInfo
 }
 
 func sanitizeFileName(path string) string {
@@ -220,18 +139,39 @@ func promptUserForFolder(dataDir string) string {
 		for i, folder := range folders {
 			fmt.Printf("[%d] %s\n", i+1, filepath.Base(folder))
 		}
+		fmt.Println("[0] Create a new node")
 
 		var choice int
 		fmt.Print("Enter your choice: ")
 		fmt.Scan(&choice)
 
-		if choice >= 1 && choice <= len(folders) {
+		if choice == 0 {
+			// Option to create a new node
+			var newNodeName string
+			fmt.Print("Enter the name of the new node: ")
+			fmt.Scan(&newNodeName)
+
+			// Construct the new node path
+			newNodePath := filepath.Join(dataDir, newNodeName)
+
+			// Create the new directory for the node
+			err := os.Mkdir(newNodePath, os.ModePerm)
+			if err != nil {
+				fmt.Println("Error creating new node folder:", err)
+				continue
+			}
+
+			fmt.Printf("New node '%s' created successfully.\n", newNodeName)
+			fmt.Printf("Current node is '%s'\n", newNodeName)
+			return newNodeName
+		} else if choice >= 1 && choice <= len(folders) {
 			return filepath.Base(folders[choice-1])
 		}
 
 		fmt.Println("Invalid choice. Please select a valid node.")
 	}
 }
+
 func promptUserForFile(dataDir, folder string) (string, string) {
 	for {
 		var fileName string
@@ -272,20 +212,8 @@ func getNodeFolders(dataDir, sourceFolder string) ([]string, error) {
 	return nodeFolders, nil
 }
 
-// SHA-1 hashing
-func hashSHA1(data string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(data))
-	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-// hashed to big integer
-func hashToBigInt(hash string) *big.Int {
-	hashedBytes, _ := hex.DecodeString(hash)
-	return new(big.Int).SetBytes(hashedBytes)
-}
-
 // write chunk data to a file
 func writeChunk(filePath string, data []byte) error {
+	fmt.Printf("Writing chunk to file: %s\n", filePath)
 	return os.WriteFile(filePath, data, 0644)
 }
--- main.go
+++ main.go
@@ -2,86 +2,80 @@
 
 package main
 
-import (
-	"distributed-chord/node"
-	"flag"
-	"fmt"
-	"log"
-	"os"
-)
+import "distributed-chord/fca"
 
 // "distributed-chord/node"
 
 func main() {
-	// fca.Chunker()
-	//fca.Assembler(fca.ChunkInfo{[]string{"Node2", "Node4", "Node1"}, "C__Users_saran_Documents_GitHub_File-Sharing-System-Chord-50.041_Data_Node1_file"})
-	// Parse command-line flags
-	ip := flag.String("ip", "127.0.0.1", "IP address of the node")
-	port := flag.Int("port", 8000, "Port number of the node")
-	joinAddr := flag.String("join", "", "Address of an existing node to join (format: ip:port)")
-	m := flag.Int("m", 5, "Key space size in bits")
 
-	flag.Parse()
+	fca.Chunker()
+	fca.Assembler(fca.ChunkInfo{[]string{"Node2", "Node4", "Node1"}, "C__Users_saran_Documents_GitHub_File-Sharing-System-Chord-50.041_Data_Node1_file"})
 
-	// // Create a new node instance
-	n := node.NewNode(*ip, *port, *m)
+	// chunkInfo := fca.Chunker()
 
-	// Start the gRPC server
-	err := n.StartGRPCServer()
-	if err != nil {
-		log.Fatalf("Failed to start gRPC server: %v", err)
-	}
+	// // Print the name of the original file and the node locations for each chunk
+	// if chunkInfo != nil {
+	// 	fmt.Printf("%v", chunkInfo)
+	// } else {
+	// 	fmt.Println("No chunks were created.")
 
-	// Join the network or create a new one
-	if *joinAddr != "" {
-		fmt.Printf("Node %x joining the network via %s...\n", n.ID, *joinAddr)
-		err = n.JoinNetwork(*joinAddr)
-		if err != nil {
-			log.Fatalf("Failed to join network: %v", err)
-		}
-	} else {
-		fmt.Printf("Node %x creating a new network...\n", n.ID)
-		n.InitializeFingerTable()
-	}
+	// // fca.Assembler(fca.ChunkInfo{[]string{"Node2", "Node4", "Node1"}, "C__Users_saran_Documents_GitHub_File-Sharing-System-Chord-50.041_Data_Node1_file"})
+	// // Start the gRPC server
+	// err := n.StartGRPCServer()
+	// if err != nil {
+	// 	log.Fatalf("Failed to start gRPC server: %v", err)
+	// }
 
-	// // Start periodic tasks (finger table updates and stabilization)
-	n.StartPeriodicTasks()
+	// // Join the network or create a new one
+	// if *joinAddr != "" {
+	// 	fmt.Printf("Node %x joining the network via %s...\n", n.ID, *joinAddr)
+	// 	err = n.JoinNetwork(*joinAddr)
+	// 	if err != nil {
+	// 		log.Fatalf("Failed to join network: %v", err)
+	// 	}
+	// } else {
+	// 	fmt.Printf("Node %x creating a new network...\n", n.ID)
+	// 	n.InitializeFingerTable()
+	// }
 
-	go func() {
-		for {
-			var command, key, value string
-			fmt.Println("Enter command (put/get/exit):")
-			fmt.Scanln(&command)
-			switch command {
-			case "put":
-				fmt.Println("Enter key:")
-				fmt.Scanln(&key)
-				fmt.Println("Enter value:")
-				fmt.Scanln(&value)
-				err := n.PutKey(key, value)
-				if err != nil {
-					fmt.Printf("Error putting key: %v\n", err)
-				} else {
-					fmt.Println("Key stored successfully.")
-				}
-			case "get":
-				fmt.Println("Enter key:")
-				fmt.Scanln(&key)
-				value, err := n.GetKey(key)
-				if err != nil {
-					fmt.Printf("Error getting key: %v\n", err)
-				} else {
-					fmt.Printf("Retrieved value: %s\n", value)
-				}
-			case "exit":
-				fmt.Println("Exiting...")
-				os.Exit(0)
-			default:
-				fmt.Println("Unknown command.")
-			}
-		}
-	}()
+	// // // Start periodic tasks (finger table updates and stabilization)
+	// n.StartPeriodicTasks()
 
-	// Keep the main function running
-	select {}
+	// go func() {
+	// 	for {
+	// 		var command, key, value string
+	// 		fmt.Println("Enter command (put/get/exit):")
+	// 		fmt.Scanln(&command)
+	// 		switch command {
+	// 		case "put":
+	// 			fmt.Println("Enter key:")
+	// 			fmt.Scanln(&key)
+	// 			fmt.Println("Enter value:")
+	// 			fmt.Scanln(&value)
+	// 			err := n.PutKey(key, value)
+	// 			if err != nil {
+	// 				fmt.Printf("Error putting key: %v\n", err)
+	// 			} else {
+	// 				fmt.Println("Key stored successfully.")
+	// 			}
+	// 		case "get":
+	// 			fmt.Println("Enter key:")
+	// 			fmt.Scanln(&key)
+	// 			value, err := n.GetKey(key)
+	// 			if err != nil {
+	// 				fmt.Printf("Error getting key: %v\n", err)
+	// 			} else {
+	// 				fmt.Printf("Retrieved value: %s\n", value)
+	// 			}
+	// 		case "exit":
+	// 			fmt.Println("Exiting...")
+	// 			os.Exit(0)
+	// 		default:
+	// 			fmt.Println("Unknown command.")
+	// 		}
+	// 	}
+	// }()
+
+	// // Keep the main function running
+	// select {}
 }
