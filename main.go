ee5d96a7ada8d5cda6ebf2f67eae3a08aac4e0bf
Sarang-Nambiar <101126190+Sarang-Nambiar@users.noreply.github.com>
2024-10-31 18:30:34 +0800

    chore: folder restructuring

--- main.go
+++ main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"distributed-chord/chord/node"
-	"distributed-chord/fca"
-	// "distributed-chord/node"
+	// "distributed-chord/fca"
 	// "log"
 	// "net/http"
-	// "os"
 	// "time"
 	// "distributed-chord/fca"
 	"fmt"
 	"log"
 	"os"
-	"time"
+	// "time"
 	"net"
 )
 
 func main() {
+
+	joinAddr := os.Getenv("BOOTSTRAP_ADDR")
 
 	// fca.Chunker()
 	// fca.Assembler(fca.ChunkInfo{[]string{"Node2", "Node4", "Node1"}, "C__Users_saran_Documents_GitHub_File-Sharing-System-Chord-50.041_Data_Node1_file"})
@@ -25,11 +25,11 @@ func main() {
 	// fmt.Printf("%v", *chunkInfo)
 
 	// Print the name of the original file and the node locations for each chunk
-	if chunkInfo != nil {
-		fmt.Printf("%v", chunkInfo)
-	} else {
-		fmt.Println("No chunks were created.")
-	}
+	// if chunkInfo != nil {
+	// 	fmt.Printf("%v", chunkInfo)
+	// } else {
+	// 	fmt.Println("No chunks were created.")
+	// }
 
 	n := node.Node{}
 
@@ -38,6 +38,18 @@ func main() {
 	err := n.StartGRPCServer()
 	if err != nil {
 		log.Fatalf("Failed to start gRPC server: %v", err)
+	}
+
+	// Check if the node is a bootstrap node or not
+
+	if joinAddr != "" {
+		fmt.Printf("Node %x joining the network via %s...\n", n., joinAddr)
+		err = n.JoinNetwork(joinAddr)
+		if err != nil {
+			log.Fatalf("Failed to join network: %v", err)
+		}
+	} else {
+		fmt.Printf("Node %x creating a new network...\n")
 	}
 
 	// // Join the network or create a new one
@@ -55,54 +67,54 @@ func main() {
 	// // Start periodic tasks (finger table updates and stabilization)
 
 	// Periodically start tasks every 30 seconds
-	go func() {
-		ticker := time.NewTicker(30 * time.Second)
-		defer ticker.Stop()
+	// go func() {
+	// 	ticker := time.NewTicker(30 * time.Second)
+	// 	defer ticker.Stop()
 
-		for {
-			select {
-			case <-ticker.C:
-				n.StartPeriodicTasks()
-				// n.startFingerTableUpdater()
-				// Add other periodic tasks here if needed
-			}
-		}
-	}()
+	// 	for {
+	// 		select {
+	// 		case <-ticker.C:
+	// 			n.StartPeriodicTasks()
+	// 			// n.startFingerTableUpdater()
+	// 			// Add other periodic tasks here if needed
+	// 		}
+	// 	}
+	// }()
 
-	go func() {
-		for {
-			var command, key, value string
-			fmt.Println("Enter command (put/get/exit):")
-			fmt.Scanln(&command)
-			switch command {
-			case "put":
-				fmt.Println("Enter key:")
-				fmt.Scanln(&key)
-				fmt.Println("Enter value:")
-				fmt.Scanln(&value)
-				err := n.PutKey(key, value)
-				if err != nil {
-					fmt.Printf("Error putting key: %v\n", err)
-				} else {
-					fmt.Println("Key stored successfully.")
-				}
-			case "get":
-				fmt.Println("Enter key:")
-				fmt.Scanln(&key)
-				value, err := n.GetKey(key)
-				if err != nil {
-					fmt.Printf("Error getting key: %v\n", err)
-				} else {
-					fmt.Printf("Retrieved value: %s\n", value)
-				}
-			case "exit":
-				fmt.Println("Exiting...")
-				os.Exit(0)
-			default:
-				fmt.Println("Unknown command.")
-			}
-		}
-	}()
+	// go func() {
+	// 	for {
+	// 		var command, key, value string
+	// 		fmt.Println("Enter command (put/get/exit):")
+	// 		fmt.Scanln(&command)
+	// 		switch command {
+	// 		case "put":
+	// 			fmt.Println("Enter key:")
+	// 			fmt.Scanln(&key)
+	// 			fmt.Println("Enter value:")
+	// 			fmt.Scanln(&value)
+	// 			err := n.PutKey(key, value)
+	// 			if err != nil {
+	// 				fmt.Printf("Error putting key: %v\n", err)
+	// 			} else {
+	// 				fmt.Println("Key stored successfully.")
+	// 			}
+	// 		case "get":
+	// 			fmt.Println("Enter key:")
+	// 			fmt.Scanln(&key)
+	// 			value, err := n.GetKey(key)
+	// 			if err != nil {
+	// 				fmt.Printf("Error getting key: %v\n", err)
+	// 			} else {
+	// 				fmt.Printf("Retrieved value: %s\n", value)
+	// 			}
+	// 		case "exit":
+	// 			fmt.Println("Exiting...")
+	// 			os.Exit(0)
+	// 		default:
+	// 			fmt.Println("Unknown command.")
+	// 		}
+	// 	}
+	// }()
 
 	// Keep the main function running
 	select {}
