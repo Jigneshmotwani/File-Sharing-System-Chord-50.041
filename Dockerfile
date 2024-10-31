bed1cd39742decdd36fd1044865629fb6e9dbdcf
Sarang-Nambiar <101126190+Sarang-Nambiar@users.noreply.github.com>
2024-10-31 09:39:29 +0800

    chore: initial docker config

--- Dockerfile
+++ Dockerfile
@@ -6,9 +6,10 @@ WORKDIR /usr/src/app
 COPY go.mod go.sum ./
 RUN go mod download && go mod verify
 
+# Making sure that netcat is installed for health check later on
+RUN apt-get update && apt-get install -y netcat-traditional
+
 COPY . .
 RUN go build -v -o /usr/local/bin/fts ./main.go
-
-VOLUME [ "/data" ]
 
 CMD ["fts"]
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
-	"fmt"
 )
 
+// 1. Figure out if the connection between the node and the bootstrap node is working
+// 2. Check if node logic from akash is working
+// 3. Implement a terminal input system to display the finger table of a node, list all nodes in the ring.
+
 func main() {
-	fmt.Println("Hello, World!")
+	
 }
